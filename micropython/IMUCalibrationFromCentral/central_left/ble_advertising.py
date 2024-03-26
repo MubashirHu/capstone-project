2c2dc0bd40d1afcf82ebb6b189a30a5f7390496e
MubashirHu <hussainmuby@gmail.com>
2024-03-26 11:58:33 -0600

    Able to catch value error in ble_advertising

--- micropython/IMUCalibrationFromCentral/central_left/ble_advertising.py
+++ micropython/IMUCalibrationFromCentral/central_left/ble_advertising.py
@@ -79,12 +79,17 @@ def decode_name(payload):
 
 def decode_services(payload):
     services = []
-    for u in decode_field(payload, _ADV_TYPE_UUID16_COMPLETE):
-        services.append(bluetooth.UUID(struct.unpack("<h", u)[0]))
-    for u in decode_field(payload, _ADV_TYPE_UUID32_COMPLETE):
-        services.append(bluetooth.UUID(struct.unpack("<d", u)[0]))
-    for u in decode_field(payload, _ADV_TYPE_UUID128_COMPLETE):
-        services.append(bluetooth.UUID(u))
+    try:
+        for u in decode_field(payload, _ADV_TYPE_UUID16_COMPLETE):
+            services.append(bluetooth.UUID(struct.unpack("<h", u)[0]))
+        for u in decode_field(payload, _ADV_TYPE_UUID32_COMPLETE):
+            services.append(bluetooth.UUID(struct.unpack("<d", u)[0]))
+        for u in decode_field(payload, _ADV_TYPE_UUID128_COMPLETE):
+            services.append(bluetooth.UUID(u))
+    except ValueError:
+        print("Value error", ValueError)
+        pass
+    
     return services
 
 
